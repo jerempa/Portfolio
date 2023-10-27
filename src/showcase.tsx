c1f3f9257efe3720df38a860e6bd80f22f1dbe9c
jerep <m pip install --upgrade pip>
2023-10-27 19:31:51 +0300

    Changed html and css

--- src/showcase.tsx
+++ src/showcase.tsx
@@ -10,35 +10,35 @@ function Showcase() {
 
     return (
         <section id="showcase">
-        <div className="container mt-5 pt-5">
-            <div className="row">
-                <div className="col-lg-10">
-                    <h1 className="text-left">Showcase</h1>
-                        <h2 className="experience">Articles written for Ruotuväki online</h2>
-                            <div className="cv-font">
-                                <ul>
-                                    <li> <a href="https://ruotuvaki.fi/-/arktisen-koulutuksen-karkiosaaja-40-vuotta" target="_blank">Arktisen koulutuksen kärkiosaaja 40 vuotta</a> </li>
-                                    <li> <a href="https://ruotuvaki.fi/-/1951245/vuoden-2019-ansioituneet-sotilasurheilijat-palkittiin" target="_blank">Vuoden 2019 ansioituneet sotilasurheilijat palkittiin</a> </li>
-                                    <li> <a href="https://ruotuvaki.fi/-/suomen-ja-ruotsin-ilmavoimat-yhdistavat-jalleen-voimansa" target="_blank">Suomen ja Ruotsin ilmavoimien yhteistoimintaharjoitus</a> </li>
-                                </ul>
-                            </div>
-                </div>
-                <div className="col-lg-10">
-                    <h2 className="programming"> Programming </h2>
+        <h1 style={{  position: "relative", "left": "950px", fontSize: "60px"}}>Showcase</h1>
+        <div className="row">
+            <div className="col-lg-10">
+                    <h2 className="programming">Articles written for Ruotuväki online</h2>
+                        <div className="programming-font">
+                            <ul>
+                                <li> <a href="https://ruotuvaki.fi/-/arktisen-koulutuksen-karkiosaaja-40-vuotta" target="_blank">Arktisen koulutuksen kärkiosaaja 40 vuotta</a> </li>
+                                <li> <a href="https://ruotuvaki.fi/-/1951245/vuoden-2019-ansioituneet-sotilasurheilijat-palkittiin" target="_blank">Vuoden 2019 ansioituneet sotilasurheilijat palkittiin</a> </li>
+                                <li> <a href="https://ruotuvaki.fi/-/suomen-ja-ruotsin-ilmavoimat-yhdistavat-jalleen-voimansa" target="_blank">Suomen ja Ruotsin ilmavoimien yhteistoimintaharjoitus</a> </li>
+                            </ul>
+                        </div>
+            </div>
+        </div>
+        <div className="row">
+            <div className="column">
+                <h2 className="programming"> Programming </h2>
                     <p className="programming-font">
                         The project I have done both with Python and JavaScript can be found on my <a href="https://github.com/jerempa" target="_blank">Github</a>.
                         There are projects that are related to course tasks but most of them are hobby projects of different sizes. The Python projects are mostly built from scratch
                         by myself (including help from Stackoverflow and such) and one JS project is based on source code by other people. The source code for this portfolio
                         can be found <a href="https://github.com/jerempa/Portfolio" target="_blank">here</a>.
                     </p>
-                </div>
-                <div className="col-lg-12">
-                    <h2 style={{ position: "relative", left: "375px", top: "-220px", fontSize: "50px" }}> Snapshots </h2>
-                    <div className="row">
-                        <img src={Ravit} className="snapshots" alt="" />
-                        <figcaption style={{position: "relative", left: "-600px", top: "-220px", width: "1000px", fontSize: "30px" }}>Fig 1. Drinking game "Ravit". Source code can be found <a href="https://github.com/jerempa/Ravit" target="_blank">here</a>.</figcaption>
-                    </div>
-                </div>
+            </div>
+        </div>
+        <div className="row">
+            <h2 style={{ position: "relative", fontSize: "50px" }}> Snapshots </h2>
+            <div className="column">
+                <img src={Ravit} className="snapshots" alt="" />
+                <figcaption style={{position: "relative", fontSize: "30px" }}>Fig 1. Drinking game "Ravit". Source code can be found <a href="https://github.com/jerempa/Ravit" target="_blank">here</a>.</figcaption>
             </div>
         </div>
         <div className="row">
