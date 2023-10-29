b1073ecf274a8930385df7913add9706182665b8
jerep <m pip install --upgrade pip>
2023-10-29 14:11:27 +0200

    Changed the about section

--- src/about.tsx
+++ src/about.tsx
@@ -6,7 +6,7 @@ function About() {
 
   return (
     <section id="home">
-      <h1 style={{  position: "relative", "left": "1350px", "top": "200px", fontSize: "60px"}}>About Me</h1>
+      <h1 style={{  position: "relative", "left": "1350px", "top": "150px", fontSize: "60px"}}>About Me</h1>
       <div className="row">
         <div className="column" >
           <img src={myImage} className="imgabout" alt="" />
@@ -16,10 +16,33 @@ function About() {
                 My name is Jere Pankka and I'm a M.Sc. Information and Service Management (ISM) student at Aalto University.
                 Completing Information and Service Management as major and Computer Science as a minor
                 in my Bachelor's degree has brought me strong expertise in business and data analysis as
-                well as intermediate knowledge in software development. The basics have come through the degree
-                , however programming as a hobby has brought most of my skillset. 
-                Learnt technical skills include but are not limited to: 
+                well as intermediate knowledge in web development. The basics have come through the degree
+                , however programming as a hobby has brought most of my skillset. I'm eager to learn more about new technologies and being able to work
+                with data. 
+                Learnt technical skills include but are not limited to:
             </p>
+                <table style={{position: "relative", "left": "-400px", "top": "150px"}}>
+                    <tr>
+                        <th>Python</th>
+                        <th>Full stack development</th>
+                        <th>Other</th>
+                    </tr>
+                    <tr>
+                        <td>Pandas, Numpy</td>
+                        <td>React, Redux</td>
+                        <td>SQL</td>
+                    </tr>
+                    <tr>
+                        <td>Scikit-learn, Matplotlib</td>
+                        <td>Node.js</td>
+                        <td>Power BI</td>
+                    </tr>
+                    <tr>
+                        <td>Requests, Beautiful Soup etc.</td>
+                        <td>MongoDB etc.</td>
+                        <td>Project management etc.</td>
+                    </tr>
+                </table>
         </div>
       </div>
     </section>
