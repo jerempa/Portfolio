2ebabbe8bec5a1a588b3e99714c62e026b2f3af1
jerep <m pip install --upgrade pip>
2023-10-30 19:34:53 +0200

    Translations

--- src/cards.tsx
+++ src/cards.tsx
@@ -13,14 +13,14 @@ export interface ITranslate {
 function Card({translate}: ITranslate) {
 
   return (
-    <div className="container mt-3">
+    <div className="container mt-3" style= {{"paddingBottom": "3vh"}}>
         <div className="row"> 
         <div className="col-lg-4 mt-4">
             <div className="card" style={{"width": "100%", "height": "100%"}}>
                 <img className="card-img-top" src={Ravit} alt="Ravit" style={{"width" :" 100%"}}/>
                 <div className="card-body">
                     <h4 className="card-title">Ravit</h4>
-                    <p className="card-text">Program made with React and TypeScript that is an electronic version of the drinking game called Ravit.</p>
+                    <p className="card-text">{translate("ravit_desc")}</p>
                     <div className="text-center">
                         <a href="https://github.com/jerempa/Ravit" className="btn btn-success" target="_blank">Repository</a>
                     </div>
@@ -32,7 +32,7 @@ function Card({translate}: ITranslate) {
                 <img className="card-img-top" src={RA2} alt="Rentapartmentfinder" style={{"width" :" 100%"}}/>
                 <div className="card-body">
                     <h4 className="card-title">Rent apartment finder</h4>
-                    <p className="card-text">Python program that searches apartments from Oikotie based on criteria given.</p>
+                    <p className="card-text">{translate("apartmentfinder_desc")}</p>
                     <div className="text-center">
                         <a href="https://github.com/jerempa/Rent-apartment-finder" className="btn btn-success" target="_blank">Repository</a>
                     </div>
@@ -44,7 +44,7 @@ function Card({translate}: ITranslate) {
                 <img className="card-img-top" src={Kide} alt="Kide" style={{"width" :" 100%"}}/>
                 <div className="card-body">
                     <h4 className="card-title">Kide.app helper</h4>
-                    <p className="card-text">Python program for buying tickets from Kide.app. Has both auto-clicker version done with Selenium as well as API version done with requests and bs4.</p>
+                    <p className="card-text">{translate("kide_desc")}</p>
                     <div className="text-center" style={{"position": "relative"}}>
                         <a href="https://github.com/jerempa/Kideapphelper" className="btn btn-success" target="_blank">Repository</a>
                     </div>
