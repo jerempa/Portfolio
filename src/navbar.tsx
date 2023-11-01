96d0034feacafd8a69dcbd572546d911b3173ea4
jerep <m pip install --upgrade pip>
2023-11-01 20:14:37 +0200

    Small fixes to responsiveness

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -14,10 +14,12 @@ function NavBar({translate}: ITranslate) {
     return (
         <nav className="navbar navbar-expand-lg fixed-top navbarScroll" >
         <LanguageSwitcher />
+        <div className="d-flex justify-content-end"> 
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+        </div>
             <div className="container">
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto">
                         <li className="nav-item active">
