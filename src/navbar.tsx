0a791a87216a4207f920decb8e8fdf93d9326474
jerep <m pip install --upgrade pip>
2023-11-08 17:12:53 +0200

    Fixed navbar issue

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -16,35 +16,40 @@ export interface ITranslate {
 
 function NavBar({translate}: ITranslate) {
 
-    const [isNavOpen, setIsNavOpen] = useState(false);
+    const [expanded, setExpanded] = useState(false);
 
     const closeNavMenu = () => {
-      setIsNavOpen(false);
+        console.log(expanded)
+        if (expanded) {
+            setExpanded(false);}
+        else {
+            setExpanded(true);
+        }
     };
     return (
-        <nav className="navbar navbar-expand-lg fixed-top navbarScroll" >
+        <nav className="navbar navbar-expand-lg fixed-top navbarScroll">
             <LanguageSwitcher />
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" style={{"position": "relative", "right": "13vw"}}>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" style={{"position": "relative", "right": "13vw"}} onClick={() => setExpanded(!expanded)}>
                 <span className="navbar-toggler-icon"></span>
             </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className={`collapse navbar-collapse ${expanded ? 'show' : ''}`} id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#home">{translate("homenav")}</a>
+                            <a className="nav-link" href="#home" onClick={() => closeNavMenu()}>{translate("homenav")}</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#cv">CV</a>
+                            <a className="nav-link" href="#cv" onClick={() => closeNavMenu()}>CV</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#showcase">Portfolio</a>
+                            <a className="nav-link" href="#showcase" onClick={() => closeNavMenu()}>Portfolio</a>
                         </li>
                         <li className="nav-item">
-                            <a href="https://github.com/jerempa" target="_blank">
+                            <a href="https://github.com/jerempa" target="_blank" onClick={() => closeNavMenu()}>
                                 <FontAwesomeIcon icon={faGithub} style={{ position: "relative",  paddingTop: "1vh", fontSize: "40px", color: "#070709" }} />
                             </a>
                         </li>
                         <li className="nav-item">
-                            <a href="https://www.linkedin.com/in/jere-pankka-806003289/" target="_blank">
+                            <a href="https://www.linkedin.com/in/jere-pankka-806003289/" target="_blank" onClick={() => closeNavMenu()}>
                                 <FontAwesomeIcon icon={faLinkedin} style={{ position: "relative", paddingTop: "1vh", fontSize: "40px", color: "#0085AE" }} />
                             </a>
                         </li>
