f193713a2903814f044a631bd737eab4ff802b2a
jerep <m pip install --upgrade pip>
2023-11-10 15:31:58 +0200

    More pictures for carousel

--- src/carousel.tsx
+++ src/carousel.tsx
@@ -8,6 +8,10 @@ import './styles/App.css';
 import MOOC1 from './images/MOOC1.jpg';
 import MOOC_frontend from './images/MOOC_frontend.jpg';
 import MOOC_backend from './images/MOOC_backend.jpg';
+import request_py from './images/request_py.jpg';
+import validation_py from './images/validation_py.jpg';
+import other_py from './images/other_py.jpg';
+
 
 
 function Carousel() {
@@ -26,6 +30,18 @@ function Carousel() {
           {
             "src": MOOC_backend,
             "alt": "MOOC3"
+          },
+          {
+            "src": request_py,
+            "alt": "request_py"
+          },
+          {
+            "src": validation_py,
+            "alt": "validation_py"
+          },
+          {
+            "src": other_py,
+            "alt": "other_py"
           }
         ]
       }
@@ -39,38 +55,39 @@ function Carousel() {
     };
 
     return (
-        <div className="container mt-3"> 
-            <div className="carousel">
-            <BsArrowLeftCircleFill onClick={prevSlide} className="arrow arrow-left" />
-            {data.images.map((item, idx) => {
-                return (
-                <img
-                    src={item.src}
-                    alt={item.alt}
-                    key={idx}
-                    className={slide === idx ? "slide" : "slide slide-hidden"}
-                />
-                );
-            })}
-            <BsArrowRightCircleFill
-                onClick={nextSlide}
-                className="arrow arrow-right"
-            />
-            <span className="indicators">
-                {data.images.map((_, idx) => {
-                return (
-                    <button
-                    key={idx}
-                    className={
-                        slide === idx ? "indicator" : "indicator indicator-inactive"
-                    }
-                    onClick={() => setSlide(idx)}
-                    ></button>
-                );
+        <div className="container mt-3" style= {{"paddingBottom": "3vh"}}>
+            <h3 className="text-center">Code snippets</h3> 
+                <div className="carousel">
+                <BsArrowLeftCircleFill onClick={prevSlide} className="arrow arrow-left" />
+                {data.images.map((item, idx) => {
+                    return (
+                    <img
+                        src={item.src}
+                        alt={item.alt}
+                        key={idx}
+                        className={slide === idx ? "slide" : "slide slide-hidden"}
+                    />
+                    );
                 })}
-            </span>
-            </div>
-            </div>
+                <BsArrowRightCircleFill
+                    onClick={nextSlide}
+                    className="arrow arrow-right"
+                />
+                <span className="indicators">
+                    {data.images.map((_, idx) => {
+                    return (
+                        <button
+                        key={idx}
+                        className={
+                            slide === idx ? "indicator" : "indicator indicator-inactive"
+                        }
+                        onClick={() => setSlide(idx)}
+                        ></button>
+                    );
+                    })}
+                </span>
+                </div>
+                </div>
     );
 };
 
