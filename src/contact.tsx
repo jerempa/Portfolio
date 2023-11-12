12a9790381a3613025f391386e4c020af1ab49a7
jerep <m pip install --upgrade pip>
2023-11-12 15:45:02 +0200

    Added translations for form

--- src/contact.tsx
+++ src/contact.tsx
@@ -39,7 +39,7 @@ export const Contact = ({ translate, onSubmit }: ContactProps) => {
       <div className="row mt-3">
         <h1 className="text-center"> Contact </h1>
         <div className="col-lg-6">
-            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum </p> 
+            <p> {translate('form_desc')}</p> 
         </div>
         <div className="col-lg-6">
             <form onSubmit={handleSubmit}>
